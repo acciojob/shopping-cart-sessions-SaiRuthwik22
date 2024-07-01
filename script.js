f86051cc4cbbd7061128e3dab71634a07c1c6cea
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-01 21:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,30 +8,54 @@ const products = [
   { id: 4, name: "Product 4", price: 40 },
   { id: 5, name: "Product 5", price: 50 },
 ];
+let cartItems = sessionStorage.getItem("cartItems")?JSON.parse(sessionStorage.getItem("cartItems")):[]
+
 
 // DOM elements
+let cartList = document.getElementById("cart-list")
 const productList = document.getElementById("product-list");
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}" onclick="addToCart(${product.id})">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartList.innerHTML = ""
+	cartItems.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}" onclick="removeFromCart(${product.id})">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  });
+	sessionStorage.setItem("cartItems",JSON.stringify(cartItems))
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	for (let i = 0; i < products.length; i++) {
+		if(productId == products[i].id){
+			cartItems.push(products[i])
+		}
+	}
+	renderCart()
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	cartItems = cartItems.filter((ele)=>ele.id !== productId)
+	renderCart()
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	cartItems = []
+	renderCart()
+}
 
 // Initial render
 renderProducts();
